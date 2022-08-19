9530eb1da0b0198454f35c4cb63d669579c521cb
Esdras Battosti <esdrasbattostisilva@gmail.com>
2022-08-19 17:11:15 -0300

    [lista 3] exercicio 7

---
+++ listas/lista3/ex7.m
@@ -0,0 +1,32 @@
+% Aluno: Esdras Battosti da Silva Ra: 2143470
+
+month = input('Type the number of the month (1 to 12): ');
+
+switch(month)
+    case 1
+        fprintf('January\n')
+    case 2
+        fprintf('February\n')
+    case 3
+        fprintf('March\n')
+    case 4
+        fprintf('April\n')
+    case 5
+        fprintf('May\n')
+    case 6
+        fprintf('June\n')
+    case 7
+        fprintf('July\n')
+    case 8
+        fprintf('August\n')
+    case 9
+        fprintf('September\n')
+    case 10
+        fprintf('October\n')
+    case 11
+        fprintf('November\n')
+    case 12
+        fprintf('December\n')
+    otherwise
+        fprintf('Invalid month\n')
+end
